86ebd904e50e3f197b94515152aeb5ee2aa79fc7
Aliaksandr <kasasha@mail.ru>
2022-10-13 21:48:04 +0300

    Task01

---
+++ Sem01/Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 1 число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 2 число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b * b == a)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
