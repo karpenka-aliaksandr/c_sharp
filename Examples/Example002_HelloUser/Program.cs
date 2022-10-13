5cb21bb3ef276993f872505a56011d08565bc7b5
Aliaksandr <kasasha@mail.ru>
2022-10-13 23:26:56 +0300

    Example002 добаввил про Csonsole.ReadLine()!

--- Examples/Example002_HelloUser/Program.cs
+++ Examples/Example002_HelloUser/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.Write("Введите ваше имя: ");
-string userName = Console.ReadLine();
+// https://telegra.ph/EHlvis-Converting-null-NET-C-09-15
+string userName = Console.ReadLine()!;
 Console.Write("Hello, ");
 Console.Write(userName);
