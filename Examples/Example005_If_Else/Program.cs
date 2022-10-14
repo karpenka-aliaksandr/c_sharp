1b0fb8806c423f20a44436a574d54bcb2642ac3d
Aliaksandr <kasasha@mail.ru>
2022-10-14 10:46:45 +0300

    Ex005_if_else

---
+++ Examples/Example005_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine()!;
+if (userName.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это Саша!");
+}
+else
+{
+    Console.WriteLine($"Привет, {userName}");
+}
+
