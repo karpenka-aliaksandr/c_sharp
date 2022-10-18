f4c2bdf3e29f2fe20f237d07c04d121253695dd5
Aliaksandr <kasasha@mail.ru>
2022-10-18 14:25:06 +0300

    Ex010 Method Array

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {6,2,7,4,3,3,4,9};
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
