320e8aa306a9ad5bae5646977c0f8ef44a4d5a92
Aliaksandr <kasasha@mail.ru>
2022-10-18 14:46:30 +0300

    Ex011 Array Library

---
+++ Examples/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write($"{collection[index]} ");
+        index++;
+    }
+
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int length = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < length)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int position = IndexOf(array,4);
+Console.WriteLine(position);
