b699a7cfa34e74357d5a9e892204df83a5bc28ef
karpenkaaliaksandr <kasasha@mail.ru>
2022-10-18 09:45:10 +0300

    Task 09

---
+++ Sem01/Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿int MaxDigit(int number)
+{
+    int firstDigit = number / 10; // 78 / 10 = 7
+    int secondDigit = number % 10; // 78 % 10 = 8
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int num = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {num}");
+
+int maxDigit = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра числа {num} = {maxDigit}");
