59f180a5aa6f54fce4291adc8431a9594d0eaa13
Aliaksandr <kasasha@mail.ru>
2022-10-22 20:32:07 +0300

    Внес правки в Task01,03,09,14,16,19,21

--- Tasks/Task14/Program.cs
+++ Tasks/Task14/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool Multiple(int dividend, int divider)
+{
+return dividend % divider == 0;
+}
+
+Console.WriteLine("Введите целое положительное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int divider1 = 7;
+int divider2 = 23;
+
+Console.WriteLine((Multiple(num, divider1) && Multiple(num, divider2)) ? "да" : "нет");
+
+
--- Tasks/Task16/Program.cs
+++ Tasks/Task16/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool Square(int number1, int number2)
+{
+return number1 * number1 == number2 || number2 * number2 == number1;
+}
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool square = Square(num1, num2);
+
+Console.Write(square?"Да":"Нет");
