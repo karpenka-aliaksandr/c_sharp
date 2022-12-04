6dc8888e1204d3cd2a833f778867ad51fa925457
Aliaksandr <kasasha@mail.ru>
2022-12-04 13:47:15 +0300

    Изменил заполнение неповторяющихся чисел

--- Tasks/Task60/Program.cs
+++ Tasks/Task60/Program.cs
@@ -4,14 +4,19 @@
 // 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
 // 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
 
-int[,,] CreateMatrixRndUniqueInt(int rows, int colums, int depth, int min, int max)
+int[,,] CreateMatrixRndUniqueInt(int rows, int colums, int depth)
 {
-    int[] uniqueNumbersArray = new int[max - min];
+    int[] uniqueNumbersArray = new int[180];
     int len = uniqueNumbersArray.Length;
-    for (int i = 0; i < len; i++)                       //заполняем массив возможных случайных чисел
+    for (int i = 0; i < len / 2; i++)                       //заполняем массив возможных случайных чисел
     {
-        uniqueNumbersArray[i] = min + i;
+        uniqueNumbersArray[i] = i - 99;
     }
+    for (int i = len / 2; i < len; i++)                       //заполняем массив возможных случайных чисел
+    {
+        uniqueNumbersArray[i] = i - 80;
+    }
+
     int[,,] matr = new int[rows, colums, depth];
     Random rnd = new Random();
     int count = rnd.Next(0, len - 1);                   //индекс массива случайных чисел 
@@ -56,11 +61,9 @@ void PrintCubeMatrix(int[,,] cubeMatr)
 
 Console.Clear();
 int minRowsColumnsDepth = 3;
-int maxRowsColumnsDepth = 5;
-int minUniqueNumber = 10;
-int maxUniqueNumber = 99;
+int maxRowsColumnsDepth = 8;
 Console.WriteLine($"Программа создает двумерных массива cо случайным (от {minRowsColumnsDepth} до {maxRowsColumnsDepth}) количеством строк, столбцов и глубиной");
-Console.WriteLine($"и заполняет случайными неповторяющимися числами [{minUniqueNumber};{maxUniqueNumber}].");
+Console.WriteLine($"и заполняет случайными неповторяющимися двузначными числами.");
 Console.WriteLine("Далее программа построчно выводит массив, добавляя индексы каждого элемента.");
 
 Random rnd = new Random();
@@ -68,11 +71,11 @@ Random rnd = new Random();
 int m = rnd.Next(minRowsColumnsDepth, maxRowsColumnsDepth + 1);
 int n = rnd.Next(minRowsColumnsDepth, maxRowsColumnsDepth + 1);
 int k = rnd.Next(minRowsColumnsDepth, maxRowsColumnsDepth + 1);
-if (m * n * k > maxUniqueNumber - minUniqueNumber)
-    Console.WriteLine($"Размер массива ({m},{n},{k}) превысил количество уникальных элементов");
+if (m * n * k > 180)
+    Console.WriteLine($"Размер массива ({m},{n},{k}) превысил количество неповторяющихся двузначных чисел.");
 else
 {
-    int[,,] cubeMatrix = CreateMatrixRndUniqueInt(m, n, k, minUniqueNumber, maxUniqueNumber);
+    int[,,] cubeMatrix = CreateMatrixRndUniqueInt(m, n, k);
     Console.WriteLine();
     Console.WriteLine($"Размер массива ({m},{n},{k})");
     Console.WriteLine();
