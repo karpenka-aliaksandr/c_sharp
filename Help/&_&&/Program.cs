62385bca0a46af21617c5afd6c1e9a61f0cb7d0c
Aliaksandr <kasasha@mail.ru>
2023-01-17 08:53:20 +0300

    некоторые особенности C# добавил в Help

---
+++ Help/&_&&/Program.cs
@@ -0,0 +1,27 @@
+﻿bool Question_one(int[] a) {
+        a[0]=20;
+        return false;  }
+bool Question_two(int[] a) {
+        a[0]=30;
+        return true; }
+
+int[] a = {10};
+if (Question_one(a) && Question_two(a)) {
+        Console.WriteLine("Условие && выполнилось"); }
+Console.WriteLine("&& a[0]="+a[0]);
+a[0] = 10;
+if (Question_one(a) & Question_two(a)) {
+        Console.WriteLine("Условие && выполнилось"); }
+Console.WriteLine("& a[0]="+a[0]);
+a[0] = 10;
+if (Question_two(a) || Question_one(a)) 
+    {
+        Console.WriteLine("Условие || выполнилось");
+    }
+Console.WriteLine("|| a[0]="+a[0]);
+a[0] = 10;
+if (Question_two(a) | Question_one(a)) 
+    {
+        Console.WriteLine("Условие | выполнилось");
+    }
+Console.WriteLine("| a[0]="+a[0]);
---
+++ Help/Int_вопрос/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string? a = Console.ReadLine();
+int? n = null;
+// if (!(a == null)){
+//     n = Convert.ToInt32(a);
+// }
+Console.WriteLine(n);
